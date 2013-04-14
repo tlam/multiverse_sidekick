586af1c8eea185921e28cc5b8888906c49ea1e9c
Thierry Lam <lamthierry@gmail.com>
2013-04-13 22:22:04 -0400

    Creating form to add heroes

--- games/admin.py
+++ games/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 
 from games.models import ActiveHero, Game
 
+class ActiveHeroInline(admin.TabularInline):
+    model = ActiveHero
+
+
+class GameAdmin(admin.ModelAdmin):
+    inlines = [
+        ActiveHeroInline,
+    ]
+
 
 admin.site.register(ActiveHero)
-admin.site.register(Game)
+admin.site.register(Game, GameAdmin)
--- games/views.py
+++ games/views.py
@@ -2,6 +2,8 @@ from django.forms.models import modelformset_factory
 from django.shortcuts import render
 
 from games.forms import GameForm
+from games.models import ActiveHero
+from heroes.models import Hero
 
 
 def create(request):
@@ -13,6 +15,14 @@ def create(request):
             game = form.save(commit=False)
             game.profile = request.user
             game.save()
+            heroes = Hero.objects.filter(pk__in=form.cleaned_data['heroes'])
+            for hero in heroes:
+                ActiveHero.objects.create(
+                    hero=hero,
+                    game=game,
+                    hp=hero.starting_hp,
+                )
+            
     else:
         form = GameForm()
 
